c1889c015f4e96ecd31e684976cb18621389fa62
Alexey Chernov <leshachernov200@mail.ru>
2025-07-28 18:20:30 +1000

    navbar ready

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,10 @@
-export default function App(){
-  return()
+import NavBar from '../widgets/NavBar/NavBar'
+import styles from './styles/App.module.scss'
+
+export default function App() {
+	return (
+		<div className={styles.app}>
+			<NavBar />
+		</div>
+	)
 }
---
+++ src/widgets/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { CiBoxList, CiMap, CiSettings } from "react-icons/ci";
+import { TiWeatherCloudy, TiWeatherWindyCloudy } from "react-icons/ti";
+import styles from './NavBar.module.scss';
+
+export default function NavBar(){
+  const [isActive, setIsActive] = useState(0)
+  return(
+    <nav className={styles.navbar}>
+      <ul className={styles.navbar__list}>
+        <TiWeatherWindyCloudy  className={styles.navbar__logo}/>
+        <TiWeatherCloudy onClick={() => setIsActive(0)} className={ isActive === 0 ? styles.list__item_active : styles.list__item}/>
+        <CiBoxList onClick={() => setIsActive(1)} className={ isActive === 1 ? styles.list__item_active : styles.list__item}/>
+        <CiMap onClick={() => setIsActive(2)} className={ isActive === 2 ? styles.list__item_active : styles.list__item}/>
+      </ul>
+      <CiSettings onClick={() => setIsActive(3)} className={ isActive === 3 ? styles.navbar__item_active : styles.navbar__item}/>
+    </nav>
+  )
+}
