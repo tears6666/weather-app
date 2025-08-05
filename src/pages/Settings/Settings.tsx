aea74162135ef4726a68d7f1b3fc0ea0292e44b0
Alexey Chernov <leshachernov200@mail.ru>
2025-08-05 16:55:10 +1000

    pre-finish commit

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,10 +1,20 @@
+import { Switch } from '@/components/ui/switch'
 import styles from './Settings.module.scss'
-
-export default function Settings(){
-  return(
-    <div className={styles.settings}>
-      <div className={styles.settings__theme}>
-      </div>
-    </div>
-  )
+export default function Settings() {
+	return (
+		<div className={styles.settings}>
+			<h1 className={styles.settings__title}>Settings</h1>
+			<div className={styles.settings__theme}>
+				<div className={styles.theme__content}>
+					<p className={styles.content__change}>Change color theme</p>
+					<p className={styles.content__text}>
+						Change color theme(black or white)
+					</p>
+				</div>
+				<div className={styles.theme__inront}>
+					<Switch />
+				</div>
+			</div>
+		</div>
+	)
 }
